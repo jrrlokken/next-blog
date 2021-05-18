a0d8e984a0b71f0c095b33a7b31e198eaf27395a
Joshua Lokken <joshualokken@pm.me>
2021-05-18 12:44:53 -0500

    Removed date component from index and id pages for blogs, as it was breaking

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
@@ -23,16 +23,26 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <>
           <p>
-          Hi, I'm Joshua, a software engineer, cook and music enthusiast.
-          <br />
-          My code is on <a href="https://github.com/jrrlokken" rel="noreferrer" target="blank">Github</a>.
-          <br />
-          This is my <span id="blog-header">blog</span>.
+            Hi, I'm Joshua, a software engineer, cook and music enthusiast.
+            <br />
+            My code is on{' '}
+            <a
+              href="https://github.com/jrrlokken"
+              rel="noreferrer"
+              target="blank"
+            >
+              Github
+            </a>
+            .
+            <br />
+            This is my <span id="blog-header">blog</span>.
           </p>
         </>
         <hr />
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} posts-section`}>
+      <section
+        className={`${utilStyles.headingMd} ${utilStyles.padding1px} posts-section`}
+      >
         {/* <h2 className={utilStyles.headingLg}>Blog</h2> */}
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -42,12 +52,13 @@ export default function Home({ allPostsData }) {
               </Link>
               <br />
               <small className={utilStyles.lightItalic}>
-                <Date dateString={date} />
+                {/* <Date dateString={date} /> */}
+                {date}
               </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,24 +1,24 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
@@ -30,10 +30,11 @@ export default function Post({ postData }) {
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightItalic}>
-          <Date dateString={postData.date} />
+          {/* <Date dateString={postData.date} /> */}
+          {date}
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  )
+  );
 }
